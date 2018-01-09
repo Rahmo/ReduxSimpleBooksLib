12a2308136e85eff67140219b3a935b60a13e9d0
Abdulrahman Alzahrani <Abdulrahman.Alzahrani@morningstar.com>
2018-01-09 17:53:57 -0600

    redcuer and containers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
+import  BooksReducer  from './reducerBooks'
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  // state: (state = {
+  //   books: BooksReducer
+  // }) => state
+  books: BooksReducer
 });
 
 export default rootReducer;
---
+++ src/reducers/reducerBooks.js
@@ -0,0 +1,7 @@
+
+export default function (){
+ return [ 
+         {title:'the good book'},
+         {title: 'another One'}
+        ]
+}
