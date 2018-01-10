01626c25ba4ce931c597f146556496c2954c60a1
Abdulrahman Alzahrani <Abdulrahman.Alzahrani@morningstar.com>
2018-01-10 11:03:49 -0600

    action creator in the container

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,10 +1,17 @@
-import React, { component, Component } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import { selectBook } from '../actions/index'
+import { bindActionCreators } from 'redux'
 class Booklist extends Component {
     renderList(){
         return this.props.books.map((book)=>{
             return (
-                <li key={book.title} className="list-group-item">{book.title}</li>
+                <li
+                key={book.title}
+                onClick={() => this.props.selectBook(book)}    
+                className="list-group-item">
+                {book.title}
+                </li>
             );
         });
     }
@@ -23,4 +30,15 @@ function mapStateToProps(state){
     };
 }
 
-export default connect(mapStateToProps)(Booklist); 
+//it specifcly returns an action
+//whenever select called, the result should be passed to all reducers
+//anything returned here get passes to the container as props
+function mapDispatchToProps(dispatch){
+    //whenever selectBook is called, the result should be passed to all our reducers
+    return bindActionCreators({selectBook: selectBook}, dispatch);
+}
+
+//Promote Booklist from a component to a container - it needs to know 
+//about this new distantch method. selectBook. make it available
+//as a prop
+export default connect(mapStateToProps,mapDispatchToProps)(Booklist); 
